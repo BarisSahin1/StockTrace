49dac27e1e95c925124688d46ece0b0dd08ee33b
BarisSahin <sahinbarisdev@gmail.com>
2022-05-04 20:48:48 +0300

    Demo stock lookup form,users controller and view is added

--- Gemfile
+++ Gemfile
@@ -33,7 +33,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 #gem for stock apı
 gem 'iex-ruby-client'
-
+#gem for icons 
+gem "font-awesome-rails"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,9 @@
 class Stock < ApplicationRecord
 	
-	def self.new_lookup (ticker_sym) 
+	def self.new_lookup(ticker_sym) 
 
 		client = IEX::Api::Client.new(
-		  publishable_token: 'pk_b57058a8feac469496f69b1eaf7e8db9',
+		  publishable_token: "pk_b57058a8feac469496f69b1eaf7e8db9",
 		  secret_token: 'secret_token',
 		  endpoint: 'https://cloud.iexapis.com/v1'
 		)
