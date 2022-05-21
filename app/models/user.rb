cb0c5c98b27595eef3421f6f72d9ea829974d808
BarisSahin <sahinbarisdev@gmail.com>
2022-05-21 23:00:04 +0300

    Search methods are added

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
   def stock_already_tracked?(ticker_sym)
     stock = Stock.check_db(ticker_sym)
@@ -25,9 +25,33 @@ class User < ApplicationRecord
   end
 
 
-   def full_name
+  def full_name
     return "#{first_name} #{last_name}" if first_name || last_name
     "Anonymous"
   end
 
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+  
+  
 end
